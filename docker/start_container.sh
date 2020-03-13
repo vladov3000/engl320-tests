b29a4df3a1c9fb2c1bc5e3e47d3a1dd3dcf6722f
vlad <vladov3000@gmail.com>
2020-03-13 16:06:45 -0700

    Docker update

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #port 8080 is mapped to port 3000 inside the container
-docker run --rm -p 8080:3000 engl320-tests
+docker run --rm -v ~/engl320-tests/:/engl320-tests -p 8080:3000 engl320-tests
